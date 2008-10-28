f59ca47ea04bb014f7256630beeb41c6e8652f94
Phill@Work <PhillWork@PHILLIP.(none)>
2008-10-27 21:51:08 -0400

    Finally getting the hang of this Rspec stuff. Still a little work to do on the date parse tests.

--- test/date_spec.rb
+++ test/date_spec.rb
@@ -1,12 +1,48 @@
-# 
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
- 
 require 'gedcom'
 include GEDCOM
 
 describe Date do
   before(:each) do
+    @date = GEDCOM::Date.new("1 APRIL 2008")
+    @date_range_from = GEDCOM::Date.new("FROM APRIL 2007 TO JUNE 2008")
+    @date_range_between = GEDCOM::Date.new("BETWEEN 1 JANUARY 1970 AND 1 APRIL 2008")
+    @date_bc = GEDCOM::Date.new("25 JANUARY 1 BC")
+    @date_year_span = GEDCOM::Date.new("1 APRIL 2007/08")
   end
   
+  ## ! Could definitely stand to beef this test up. About, Estimated, etc. 
+  ##   Lot's of flags to test.
+  it "makes flags available" do
+    (@date_range_from.format & GEDCOM::Date::FROMTO).should != 0
+    (@date_range_between.format & GEDCOM::Date::BETWEEN).should != 0
+  end
+  
+  it "does comparison" do
+    (@date <=> @date_bc).should == 1
+    (@date_bc <=> @date).should == -1
+    (@date <=> @date).should == 0
+  end
+  
+  it "gets first and last date from ranges" do
+    @date_range_from.is_range?.should == true
+    @date_range_between.is_range?.should == true
+    
+    @date_range_from.first.nil?.should == false
+    @date_range_from.last.nil?.should == false
+    @date_range_between.first.nil?.should == false
+    @date_range_between.last.nil?.should == false
+    
+    (@date_range_from.first <=> @date_range_from.last).should == -1
+    (@date_range_between.first <=> @date_range_between.last).should == -1
+  end
+  
+  # to_s currently works differently in the Ruby vs. C extension
+  # code, therefore this test is failing (in C)
+  it "converts to string" do
+    @date.to_s.should == "1 Apr 2008"
+    @date_range_from.to_s.should == "from 0 Apr 2007 to 0 Jun 2008"
+    @date_range_between.to_s.should == "bet 1 Jan 1970 and 1 Apr 2008"
+    @date_bc.to_s.should == "25 Jan 1 BC"
+    @date_year_span.to_s.should == "1 Apr 2007-8"
+  end
 end
--- test/datepart_spec.rb
+++ test/datepart_spec.rb
@@ -1,7 +1,3 @@
-# 
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
- 
 require 'gedcom'
 include GEDCOM
 
@@ -15,69 +11,78 @@ describe DatePart do
   end
   
   it "makes date type and flags available" do
-    return false if !(@date_range_from.first.compliance && ((@date_range_from.first.compliance & GEDCOM::DatePart::NODAY) == 0))
-    return false if !(@date_range_from.last.compliance && ((@date_range_from.last.compliance & GEDCOM::DatePart::NODAY) == 0))
-    return false if !(@date_range_from.first.calendar && ((@date_range_from.first.calendar & GEDCOM::DateType::DEFAULT) == 0))
-    return false if !(@date_range_from.last.calendar && ((@date_range_from.last.calendar & GEDCOM::DateType::DEFAULT) == 0))
-  
-    true
+    (@date_range_from.first.compliance & GEDCOM::DatePart::NODAY).should != 0
+    (@date_range_from.last.compliance & GEDCOM::DatePart::NODAY).should != 0
+    (@date_range_from.first.calendar & GEDCOM::DateType::DEFAULT).should != 0
+    (@date_range_from.last.calendar & GEDCOM::DateType::DEFAULT).should != 0
   end
 
   it "finds days" do
-    return false if !(@date.first.has_day? && (@date.first.day == 1))
+    @date.first.has_day?.should == true
+    @date.first.day.should == 1
     
-    return false if !(@date_range_between.first.has_day? && (@date_range_between.first.day == 1))
-    return false if !(@date_range_between.last.has_day? && (@date_range_between.last.day == 1))
+    @date_range_between.first.has_day?.should == true
+    @date_range_between.first.day.should == 1
     
-    return false if !(@date_bc.first.has_day? && (@date_bc.first.day == 25))
+    @date_range_between.last.has_day?.should == true
+    @date_range_between.last.day.should == 1
     
-    true
+    @date_bc.first.has_day?.should == true 
+    @date_bc.first.day.should == 25
   end
   
   it "finds months" do
-    return false if !(@date.first.has_month? && (@date.first.month == 4))
+    @date.first.has_month?.should == true
+    @date.first.month.should == 4
     
-    return false if !(@date_range_between.first.has_month? && (@date_range_between.first.month == 1))
-    return false if !(@date_range_between.last.has_month? && (@date_range_between.last.month == 4))
+    @date_range_between.first.has_month?.should == true
+    @date_range_between.first.month.should == 1
     
-    return false if !(@date_bc.first.has_month? && (@date_bc.first.month == 1))
+    @date_range_between.last.has_month?.should == true
+    @date_range_between.last.month.should == 4
     
-    true
+    @date_bc.first.has_month?.should == true
+    @date_bc.first.month.should == 1
   end
   
   it "finds years" do
-    return false if !(@date.first.has_year? && (@date.first.year == 2008))
+    @date.first.has_year?.should == true
+    @date.first.year.should == 2008
     
-    return false if !(@date_range_between.first.has_year? && (@date_range_between.first.year == 1970))
-    return false if !(@date_range_between.last.has_year? && (@date_range_between.last.year == 2008))
+    @date_range_between.first.has_year?.should == true
+    @date_range_between.first.year.should == 1970
     
-    return false if !(@date_bc.first.has_year? && (@date_bc.first.year == 1))
+    @date_range_between.last.has_year?.should == true
+    @date_range_between.last.year.should == 2008
     
-    true
+    @date_bc.first.has_year?.should == true
+    @date_bc.first.year.should == 1
   end
   
   it "finds the epoch" do
-    return false if !(@date.first.epoch == "AD")
-
-    return false if !(@date_bc.first.epoch == "BC")
-    
-    true
+    @date.first.epoch.should == "AD"
+    @date_bc.first.epoch.should == "BC"
   end  
 
   it "finds year span" do
-    return false if !(@date_year_span.first.has_year_span?)
-    
-    return false if @date.first.has_year_span?
-    
-    true
+    @date_year_span.first.has_year_span?.should == true
+    @date.first.has_year_span?.should == false
   end
   
+  # to_s currently works differently in the Ruby vs. C extension
+  # code, therefore this test is failing (in C)
   it "converts to string" do
-    return false if @date_range_from.to_s != "from 0 Apr 2007 to 0 Jun 2008"
-    return false if @date_range_from.first.to_s != "0 Apr 2007"
-    return false if @date_range_from.last.to_s != "0 Jun 2008"
+    @date.first.to_s.should == "1 Apr 2008"
     
-    true
+    @date_range_from.first.to_s.should == "0 Apr 2007"
+    @date_range_from.last.to_s.should == "0 Jun 2008"
+    
+    @date_range_between.first.to_s.should == "1 Jan 1970"
+    @date_range_between.last.to_s.should == "1 Apr 2008"
+    
+    @date_bc.first.to_s.should == "25 Jan 1 BC"
+    
+    @date_year_span.to_s.should == "1 Apr 2007-8"
   end
 
 end
